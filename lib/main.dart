c70048464c7095030eeca86db1d675368e5c66b6
Parshuram Pathave <pgames3d@gmail.com>
2023-04-27 14:36:43 +0530

    main file change

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Project',
+      title: 'Flutter Project Sandy',
       theme: ThemeData(
         // This is the theme of your application.
         //
